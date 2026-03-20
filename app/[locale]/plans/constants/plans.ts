8deb56b00dd330da928ca60089bf4050bdbb72f6
havynguyen23 <havy.nguyen142003@gmail.com>
2026-03-20 17:19:30 +0700

    fix: update plans and days status to reflect completed tasks

--- app/[locale]/plans/constants/plans.ts
+++ app/[locale]/plans/constants/plans.ts
@@ -1,4 +1,7 @@
+// Constants
 import { ROUTES } from '@/app/constants/routes';
+
+// Components
 import type { PlanStatus } from '../components/PlanCard';
 
 export type PlanStaticData = {
@@ -8,8 +11,20 @@ export type PlanStaticData = {
 };
 
 export const PLANS: PlanStaticData[] = [
-  { id: '0', status: 'in-progress', slug: ROUTES.TESTER_PLAN.PATH.split('/').pop() },
-  { id: '1', status: 'in-progress', slug: ROUTES.HTML_PLAN.PATH.split('/').pop() },
-  { id: '2', status: 'planned', slug: ROUTES.CSS_PLAN.PATH.split('/').pop() },
-  { id: '3', status: 'planned', slug: ROUTES.JS_PLAN.PATH.split('/').pop() },
+  {
+    id: '0',
+    status: 'in-progress',
+    slug: ROUTES.TESTER_PLAN.PATH.split('/').pop(),
+  },
+  {
+    id: '1',
+    status: 'in-progress',
+    slug: ROUTES.CSS_PLAN.PATH.split('/').pop(),
+  },
+  { id: '2', status: 'planned', slug: ROUTES.JS_PLAN.PATH.split('/').pop() },
+  {
+    id: '3',
+    status: 'completed',
+    slug: ROUTES.HTML_PLAN.PATH.split('/').pop(),
+  },
 ];
